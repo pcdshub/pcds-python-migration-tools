e75ac4c2c2c9d5c850e569cb176af48b754cb566
tangkong <tangkongrobert@gmail.com>
2023-09-25 11:15:58 -0700

    REF: collect protection group settings in dataclass

--- update_branch_protections.py
+++ update_branch_protections.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 
 import argparse
 import csv
+from dataclasses import dataclass, fields
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import List
 
 from update_github_settings import BranchProtection, Repository
 
@@ -23,16 +24,68 @@ all_prot = BranchProtection(pattern='*', required_status_checks=[],
                             allows_deletions=True,)
 
 
-# Map column names to protection rules
-RULE_MAP = {'master': master_protect, 'gh-pages': gh_pages_prot, 'default': all_prot}
-
-
 def str_to_bool(val: str):
     return val.lower() in ('y', 'yes', 'true')
 
 
-def parse_repo_list(repo_data_path: str) -> List[Dict[str, Any]]:
-    """ Returns {reponame: {master: bool, gh_pages: bool, all: bool}}"""
+@dataclass
+class ProtectionGroup:
+    owner: str
+    repo_name: str
+    master: bool = False
+    gh_pages: bool = False
+    default: bool = False
+
+    # Map column names to protection rules
+    RULE_MAP = {
+        'master': master_protect,
+        'gh_pages': gh_pages_prot,
+        'default': all_prot
+    }
+
+    @classmethod
+    def from_dict(cls, source: dict):
+        try:
+            flags = {f.name: str_to_bool(source[f.name]) for f in fields(cls)
+                     if f.name not in ('owner', 'repo_name')}
+        except KeyError as e:
+            print("source data malformed, aborting")
+            raise e
+
+        return cls(
+            owner=source["owner"],
+            repo_name=source["repo_name"],
+            **flags
+        )
+
+    def apply_protections(
+        self,
+        write: bool
+    ) -> None:
+        repo = Repository.from_name(owner=self.owner, repo=self.repo_name)
+
+        for prot in BranchProtection.from_repository(repo):
+            if write:
+                print("Deleting branch protection setting")
+                prot.delete()
+            else:
+                print("(dry run) Deleting branch protection setting")
+
+        rule_names = (f.name for f in fields(self)
+                      if f.name not in ('owner', 'repo_name'))
+
+        for name in rule_names:
+            if getattr(self, name, False):
+                print(f'created {name} rule for repo: {self.repo_name}')
+                if write:
+                    protection_rule = self.RULE_MAP[name]
+                    protection_rule.create(repo)
+                    print(f'-- {name} rule applied')
+                else:
+                    print('-- rule not applied')
+
+
+def parse_repo_list(repo_data_path: str) -> List[ProtectionGroup]:
     data_path = Path(repo_data_path)
     if not data_path.exists:
         print('repo data file does not exist')
@@ -40,49 +93,11 @@ def parse_repo_list(repo_data_path: str) -> List[Dict[str, Any]]:
     repo_data = []
     # Deal with extra columns
     with open(data_path, 'r') as csvfile:
-        csv_reader = csv.reader(csvfile)
+        csv_reader = csv.DictReader(csvfile)
         for row in csv_reader:
-            try:
-                repo_data.append(
-                    {'owner': row['owner'], 'repo_name': row['repo_name'],
-                     'master': str_to_bool(row['master']),
-                     'pages': str_to_bool(row['pages']),
-                     'default': str_to_bool(row['default'])}
-                )
-            except KeyError as e:
-                print('CSV File malformed, aborting')
-                raise e
-
+            data_row = ProtectionGroup.from_dict(row)
+            repo_data.append(data_row)
     return repo_data
-
-
-def apply_protections(owner: str, repo_name: str, prot_master: bool, prot_pages: bool, prot_default: bool,
-                      write: bool):
-    repo = Repository.from_name(owner=owner, repo=repo_name)
-
-    for prot in BranchProtection.from_repository(repo):
-        if write:
-            print("Deleting branch protection setting")
-            prot.delete()
-        else:
-            print("(dry run) Deleting branch protection setting")
-
-    if prot_master:
-        print(f'created master rule for repo: {repo_name}')
-        if write:
-            master_protect.create(repo)
-            print('-- master rule applied')
-    if prot_pages:
-        print(f'created gh-pages rule for repo: {repo_name}')
-        if write:
-            master_protect.create(repo)
-            print('-- pages rule applied')
-    if prot_default:
-        print(f'created default (*) rule for repo: {repo_name}')
-        if write:
-            master_protect.create(repo)
-            print('-- default (*) rule applied')
-    return
 
 
 def main(
@@ -94,22 +109,21 @@ def main(
     repo_data_path: str = "",
     write: bool = False
 ):
-    print(owner, repo_name, prot_master, prot_pages, prot_default,
-          repo_data_path, write)
-
     if repo_data_path:
         # run for each repo in list
         repo_data = parse_repo_list(repo_data_path)
-        for repo_details in repo_data:
-            apply_protections(repo_details['owner'], repo_details['repo_name'],
-                              repo_details['master'], repo_details['pages'],
-                              repo_details['default'], write=write)
+        for protection_group in repo_data:
+            protection_group.apply_protections(write=write)
+        return
+
+    if not repo_name:
         return
 
     # apply settings for a single repo
-    apply_protections(owner, repo_name, prot_master=prot_master,
-                      prot_pages=prot_pages, prot_default=prot_default,
-                      write=write)
+    protection_group = ProtectionGroup(
+        owner, repo_name, master=prot_master, gh_pages=prot_pages, default=prot_default
+    )
+    protection_group.apply_protections(write=write)
 
     return
 
@@ -124,8 +138,8 @@ def _create_argparser() -> argparse.ArgumentParser:
     """
     parser = argparse.ArgumentParser()
     # specify an owner, repo, and settings
-    parser.add_argument("owner", type=str)
-    parser.add_argument("repo_name", type=str)
+    parser.add_argument("owner", type=str, default='pcdshub', nargs='?')
+    parser.add_argument("repo_name", type=str, default='', nargs='?')
     parser.add_argument("--protect-master", action="store_true", dest='prot_master')
     parser.add_argument("--protect-pages", action="store_true", dest='prot_pages')
     parser.add_argument("--protect-default", action="store_true", dest='prot_default')
